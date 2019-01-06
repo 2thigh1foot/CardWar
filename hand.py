2b217de71ce284d8de9cb9b57c61ee96d10112fa
hmc-cod-f18 <csimon@cs.hmc.edu>
2019-01-05 21:03:02 -0600

    Started hand for real

--- hand.py
+++ hand.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 #a class that represents the hadn of cards a player has
 
 class Hand:
@@ -7,18 +9,22 @@ class Hand:
     '''
     def __init__(self, cards):
         self.inHand = []
-
+        #how are we determining what cards are in hand?
     '''
         Play the cards specified
     '''
     def play_cards(card_list):
+        #does this get rid of cards in hand and then place them in another pile?
 
     '''
         Discard specified cards
     '''
     def discard(card_list):
-    
+        #where is the discard pile
+
     '''
         Shuffle the cards in hand
     '''
     def shuffle():
+        random.shuffle(self.inHand)
+
