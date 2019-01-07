9cfc95e5c1e376d11f037f769b5bcc197ab29f1b
hmc-cod-f18 <csimon@cs.hmc.edu>
2019-01-06 20:43:26 -0600

    Added all hand.py code

--- hand.py
+++ hand.py
@@ -7,24 +7,35 @@ class Hand:
     '''
         Default constructor for a Hand
     '''
-    def __init__(self, cards):
-        self.inHand = []
-        #how are we determining what cards are in hand?
+    def __init__(self, cards=[]):
+        self.cards = cards
+        self.discarded = []
+        
     '''
         Play the cards specified
     '''
-    def play_cards(card_list):
-        #does this get rid of cards in hand and then place them in another pile?
-
+    def play_cards(num=1):
+      	cardsPlayed = []
+        
+        if(len(self.cards) < num):
+          	if(len(self.cards)+len(self.discarded) < num):
+              	return []
+            self.shuffle()
+            self.cards += self.discarded
+            self.discarded = []
+            
+        for i in range(num):
+            takenCard = self.cards.pop(0)
+            cardsPlayed.append(takenCard)
+        return cardsPlayed
+        
     '''
-        Discard specified cards
     '''
     def discard(card_list):
-        #where is the discard pile
-
+        self.discard += card_list
     '''
         Shuffle the cards in hand
     '''
     def shuffle():
-        random.shuffle(self.inHand)
+        shuffle(self.discard)
 
