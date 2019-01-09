e04cb24db2a8b06d16bc669525f2ce4afe7d12fe
Ciante Jones <ciantej@gmail.com>
2019-01-08 21:32:06 -0800

    Fixed some self stuff and made cards printable

--- deck.py
+++ deck.py
@@ -2,54 +2,60 @@ from random import shuffle, randint
 from card import Card
 # A class that represents a deck of cards
 
+
 class Deck:
 
     fullDeck = []
-    for i in range(2,15):
+    for i in range(2, 15):
         for suit in Card.suits:
-            fullDeck.append(Card(i,suit))
-              
+            fullDeck.append(Card(i, suit))
+
     '''
         Constructor for a Deck
     '''
+
     def __init__(self):
-        
-        #Declare a list of Cards that make up a Deck
-        self.cards = fullDeck[::]
+
+        # Declare a list of Cards that make up a Deck
+        self.cards = self.fullDeck[::]
         self.discard = []
-        
+
         shuffle(self.cards)
-            
+
     '''
         Draws one random card from the deck.
     '''
-    def draw_card(num=1): 
-        
+
+    def draw_card(self, num=1):
+
         removedCards = []
         for i in range(num):
             card = self.cards.pop(0)
             removedCards.append(card)
-            
+
         return removedCards
 
-    def discarded():
+    def discarded(self):
         return self.discard
     '''
         Shuffles the deck using random object shuffle.
     '''
-    def shuffle():
+
+    def shuffle(self):
         shuffle(self.cards)
-    
+
     '''
         Returns the cards in the deck.
     '''
-    def remaining():
+
+    def remaining(self):
         return self.cards
 
     '''
         Resets the deck to a full one
     '''
-    def reset():
+
+    def reset(self):
         self.lostCards = []
         self.cards = fullDeck[::]
         shuffle(self.cards)
@@ -57,11 +63,12 @@ class Deck:
     '''
         Draws a random card from the deck
     '''
-    def draw_random(num=1):
-        #Selects random card
+
+    def draw_random(self, num=1):
+        # Selects random card
         removedCards = []
         for i in range(num):
-            card = self.cards.pop(randint(0,len(self.cards)-1))
+            card = self.cards.pop(randint(0, len(self.cards)-1))
             removedCard.append(card)
             self.lostCards(card)
 
@@ -72,5 +79,6 @@ class Deck:
     '''
         Removes specified card
     '''
-    def discard_card(card):
+
+    def discard_card(self, card):
         self.discard.append(card)
--- hand.py
+++ hand.py
@@ -1,44 +1,49 @@
 from random import shuffle
 
-#a class that represents the hadn of cards a player has
+# a class that represents the hadn of cards a player has
+
 
 class Hand:
 
     '''
         Default constructor for a Hand
     '''
+
     def __init__(self, cards=[]):
         self.cards = cards
         self.discarded = []
 
-    def __len__(self):
-        return len(self.cards+self.discarded)
-        
     '''
         Play the cards specified
     '''
-    def play_cards(num=1):
-      	cardsPlayed = []
-        
+
+    def play_cards(self, num=1):
+        cardsPlayed = []
+
         if(len(self.cards) < num):
-          	if(len(self.cards)+len(self.discarded) < num):
-              	return []
+            if(len(self.cards)+len(self.discarded) < num):
+                return []
             self.shuffle()
             self.cards += self.discarded
             self.discarded = []
-            
+
         for i in range(num):
             takenCard = self.cards.pop(0)
             cardsPlayed.append(takenCard)
         return cardsPlayed
-        
-    '''
-    '''
-    def discard(card_list):
-        self.discard += card_list
-    '''
-        Shuffle the cards in hand
-    '''
-    def shuffle():
-        shuffle(self.discard)
 
+    def discard(self, card_list):
+        self.discarded += card_list
+
+    def shuffle(self):
+        '''
+        Shuffle the cards in hand
+        '''
+        shuffle(self.discarded)
+
+    # print out hands
+    def __repr__(self):
+        return str(self.cards)
+
+    def __len__(self):
+        return len(self.cards+self.discarded)
